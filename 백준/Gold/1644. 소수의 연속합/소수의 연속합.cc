3e778c9a17fda728d7dc510d10f66886cb2aee7a
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2024-05-02 17:58:06 +0900

    [Gold III] Title: 소수의 연속합, Time: 40 ms, Memory: 5712 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+vector<bool> isprime(4000001, 0);
+vector<int> prime;
+int main(){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    for(int i = 2; i <= 2000; i++){
+        if(isprime[i])continue;
+        
+        prime.push_back(i);
+        for(int j = i*2;j<=4000000;j+=i){
+            isprime[j]=true;
+        }
+    }
+
+    for(int i = prime.back()+1; i<4000000;i++){
+        if(!isprime[i])prime.push_back(i);
+    }
+
+    int l=0, r=0, n, cnt=0, sum = 0;
+    cin>>n;
+    while(l<=r){
+
+        if(sum <= n && r < prime.size())sum+=prime[r++];
+        else sum-=prime[l++];
+        
+        if(sum==n)cnt++;
+
+    }
+    cout << cnt;
+}
