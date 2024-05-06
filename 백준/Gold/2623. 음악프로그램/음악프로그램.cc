1a3ce9f9b52d6e51b6e8558fe034cbc0fd18bada
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2024-05-07 01:42:13 +0900

    [Gold III] Title: 음악프로그램, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.cc
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.cc
@@ -1,57 +1,36 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-#include<algorithm>
 using namespace std;
-
 int main(){
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     int n, m,a,b,t;
-
     cin >> n >> m;
     int indegree[n+1]={0,};
     vector<int>g[n+1];
     queue<int>q;
+    vector<int> ans;
     for(int i = 0; i < m; i++){
         cin >> t;
-        for(int j = 0; j < t; j++){
+        cin>>a;
+        for(int j = 1; j < t; j++){
             cin >> b;
-            if(j==0){
-                a=b;
-                continue;
-            }
             indegree[b]++;
             g[a].push_back(b);
             a=b;
         }
     }
-
-    for(int i = 1; i <= n; i++){
-        if(indegree[i]==0)q.push(i);
-    }
-    if(q.size()==0){
-        cout << 0;
-        return 0;
-    }
-    vector<int> ans;
+    for(int i = 1; i <= n; i++) if(indegree[i]==0)q.push(i);
     while(!q.empty()){
         int now = q.front();
         ans.push_back(now);
         q.pop();
-
         for(auto i : g[now]){
-            if(--indegree[i]==0){
-                q.push(i);
-            }
-            else if(indegree[i]<0){
-                cout << 0;
-                return 0;
-            }
+            if(--indegree[i]==0) q.push(i);
         }
     }
-    if(ans.size()<n){
-        cout << 0;
-        return 0;
+    if(ans.size()<n) cout << 0;
+    else{
+        for(auto i : ans)cout << i << "\n";
     }
-    for(auto i : ans)cout << i << "\n";
 }
