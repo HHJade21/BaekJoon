417444ae4e47ee95c4d0ea19fc6b9efc5ae4f96d
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2024-05-07 00:03:13 +0900

    [Gold III] Title: 줄 세우기, Time: 28 ms, Memory: 3820 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,44 +1,28 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-#include<algorithm>
 using namespace std;
-
 int main(){
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     int n, m, a, b;
-
     cin >> n >> m;
-    vector<int>poto(n+1, 0);
-    queue<int> potoQ;
+    vector<int>indegree(n+1, 0);
+    queue<int> q;
     vector<int>graph[n+1];
-    vector<int> ans;
-
     for(int i = 0; i < m; i++){
         cin >> a >> b;
         graph[a].push_back(b);
-        poto[b]++;
+        indegree[b]++;
     }
-
     for(int i = 1; i <= n; i++){
-        if(!poto[i]) {
-            potoQ.push(i);
-            ans.push_back(i);
+        if(!indegree[i]) q.push(i);
+    }
+    while(!q.empty()){
+        int now = q.front();
+        cout << now << " ";
+        q.pop();
+        for(auto i : graph[now]){
+            if(--indegree[i]==0) q.push(i);
         }
     }
-
-    while(!potoQ.empty()){
-        int qsize = potoQ.size();
-        for(int i = 0; i < qsize; i++){
-            int now = potoQ.front();
-            potoQ.pop();
-            for(auto j : graph[now]){
-                if(--poto[j]==0) {
-                    potoQ.push(j);
-                    ans.push_back(j);
-                }
-            }
-        }
-    }
-    for(auto i : ans)cout << i << " ";
 }
