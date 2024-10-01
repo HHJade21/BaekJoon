e8bfc5177b9b0d87ba5a600cd2e71a7f194e47f0
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2024-10-01 16:36:59 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
@@ -14,40 +14,19 @@ int main()
     for (int i=0; i<n; i++)
     {
         cin >> line;
-        if (line == num)
-        {
-            //test
-            //cout << "line == num :" << line << "\n";
-            
-            num++;
-        }
-        else
-        {
-            //test
-            //cout << "push " << line << "\n";
-
-            wait.push(line);
-        }
+        if (line == num) num++;
+        else wait.push(line);
 
         while (wait.size() > 0 && wait.top() == num)
         {
-            //test
-            //cout << "pop" << wait.top() << ", num : " << num << "\n";
-
             wait.pop();
             num++;
-            if(num>n){
-                break;
-            }
+            
+            if(num>n) break;
         }    
     }
-    // 결과 출력
-    if (wait.size() == 0)
-    {
-        cout << "Nice";
-    }
-    else
-    {
-        cout << "Sad";
-    }
+
+    if (wait.size() == 0) cout << "Nice";
+    else cout << "Sad";
+
 }
