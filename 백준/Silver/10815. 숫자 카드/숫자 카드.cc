1ea9f8c36c6fc7ad64b60c12c186067e9ae80857
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2024-10-07 22:38:13 +0900

    [Silver V] Title: 숫자 카드, Time: 196 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -9,34 +9,20 @@ vector<int>card(500000);
 
 bool bs(int k){
     int l=0, r = n-1, mid;
-
-    while(l<r){
+    while(l<=r){
         mid = (l+r)/2;
-
         if(card[mid]==k)return true;
-        else if(card[mid]<k){
-            l = mid+1;
-        }
-        else{
-            r = mid;
-        }
+        else if(card[mid]<k)l = mid+1;
+        else r = mid-1;
     }
-    if(card[l]==k)return true;
-
     return false;
 }
 
 int main(){
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
     cin >> n;
-    
-    for(int i = 0; i < n; i++){
-        cin>>card[i];
-    }
+    for(int i = 0; i < n; i++)cin>>card[i];
     sort(card.begin(), card.begin()+n);
-
-
     cin>>m;
     while(m--){
         cin>>tmp;
