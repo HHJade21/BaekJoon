4b8397f5b610db43f40ed83c8274ddd13e128184
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2024-10-22 18:25:15 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.cc
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -2,13 +2,12 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     int n=1,a,k=0;
     cin>>a;
 
-    while(n<a){
+    while(a<n||n+(k*6)<a){
+        n+=(k*6);
         k++;
-        n+=k*6;
     }
     cout<<k+1;
 
