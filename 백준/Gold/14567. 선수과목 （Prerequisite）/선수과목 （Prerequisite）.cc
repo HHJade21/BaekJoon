72f1c36d1d62f2d896d93148cd8a329238be5f37
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2024-11-10 18:42:27 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 72 ms, Memory: 4928 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
@@ -1,29 +1,21 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-#include<algorithm>
-#include<limits.h>
 using namespace std;
-
 int main(){
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     int n, m, ind[1001]={0,},cnt=0,a,b,ans[1001]={0,};
     vector<int>g[1001];
     queue<int>q;
-
     cin>>n>>m;
     for(int i = 0; i < m; i++){
         cin>>a>>b;
         ind[b]++;
         g[a].push_back(b);
     }
-
     for(int i = 1; i <= n; i++){
-        if(ind[i]==0){
-            q.push(i);
-        }
+        if(ind[i]==0) q.push(i);
     }
-
     while(q.size()){
         int size = q.size();
         cnt++;
