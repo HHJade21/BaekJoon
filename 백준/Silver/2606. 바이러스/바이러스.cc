0947aeacd4dbdb917fd3d98be60b61ebbf198e36
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2024-11-21 20:42:16 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,26 +1,41 @@
 #include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
 using namespace std;
-int n, m, graph[101][101]={0,},a,b,cnt=0,visited[101]={0,};
 
-void dfs(int now){
-    for(int i = 1; i<=n; i++){
-        if(graph[now][i] == 1 && visited[i]==0){
-            visited[i]=1;
-            cnt++;
-            dfs(i);
-        }
-    }
-}
 int main(){
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    cin >> n >> m;
+    int n, m,a,b;
+    bool visit[101]={0,};
+    int graph[101][101]={0,};
 
+    cin >> n >> m;
     for(int i = 0; i < m; i++){
         cin >> a >> b;
         graph[a][b]=1;
         graph[b][a]=1;
+
     }
-    visited[1]=1;
-    dfs(1);
+
+    visit[1]=true;
+    queue<int>bfsQ;
+    bfsQ.push(1);
+    int cnt=0;
+
+    while(!bfsQ.empty()){
+        int qsize=bfsQ.size();
+        for(int i = 0; i < qsize; i++){
+            int now = bfsQ.front();
+            for(int i = 1; i <= n; i++){
+                if(graph[now][i]==1 && !visit[i]){
+                    visit[i]=true;
+                    bfsQ.push(i);
+                    cnt++;
+                }
+            }
+            bfsQ.pop();
+        }
+    }
     cout << cnt;
 }
