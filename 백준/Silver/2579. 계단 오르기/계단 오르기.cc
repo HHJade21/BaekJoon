72b0ab854b7285bd0b4575211570df3198f01caf
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-01-18 15:30:25 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,39 +1,18 @@
 #include<iostream>
 #include<vector>
-#include<queue>
-#include<algorithm>
-#include<limits.h>
 using namespace std;
-
 int main(){
-    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     int n;
     cin >> n;
+    vector<int>s(n);
+    for(int i = 0; i < n; i++)cin>>s[i];
+    vector<int> dp(n,0);
+    dp[0]=s[0];
+    dp[1]=s[0]+s[1];
+    dp[2]=max(s[0]+s[2], s[1]+s[2]);
 
-    vector<int>stair(n, 0);
-    vector<int>dp(n,0);
-
-    for(int i = 0; i < n; i++)cin>>stair[i];
-
-    if(n==1)
-    {
-        cout << stair[0];
-        return 0;
+    for(int i = 3; i < n; i++){
+        dp[i] = max(dp[i-2]+s[i], dp[i-3]+s[i-1]+s[i]);
     }
-    else if(n==2)
-    {
-        cout << stair[0]+stair[1];
-        return 0;
-    }
-
-    dp[0] = stair[0];
-    dp[1] = stair[0]+stair[1];
-    dp[2] = max(stair[0]+stair[2], stair[1]+stair[2]);
-
-    for(int i = 3; i < n; i++)
-    {
-        dp[i]= max(dp[i-3]+stair[i-1]+stair[i], dp[i-2]+stair[i]);
-    }
-
-    cout << dp[n-1];
+    cout<<dp[n-1];
 }
