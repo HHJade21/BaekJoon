22202f22208a539e7ff37a628082791f1a116883
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-02-06 19:39:11 +0900

    [Gold V] Title: 평범한 배낭, Time: 32 ms, Memory: 396552 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -3,17 +3,22 @@ using namespace std;
 int n, k,w[101],v[101];
 int dp[101][1000001] = {};
 int main() {
-	cin >> n >> k;
-	for (int i = 1; i <= n; i++) {
-		cin >> w[i] >> v[i] ;
+	cin>>n>>k;
+
+	for(int i = 1; i <=n; i++){
+		cin>> w[i] >> v[i];
 	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-            if(j<w[i])
-                dp[i][j]=dp[i-1][j];
-            else
-			    dp[i][j] = max(dp[i - 1][j - w[i]] + v[i], dp[i - 1][j]);
+
+	for(int i = 1; i <= n; i++){//1번 물건부터 n번 물건까지 하나하나 추가하기
+		for(int j = 0; j<=k; j++){
+			if(j<w[i]){
+				dp[i][j]=dp[i-1][j];
+			}
+			else {
+				dp[i][j] = max(dp[i-1][j-w[i]]+v[i], dp[i-1][j]);
+			}
 		}
 	}
+
 	cout << dp[n][k];
 }
