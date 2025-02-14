cc288231257fd270dc64e4de09fa2670509b1fb3
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-02-14 15:57:48 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 732 ms, Memory: 70648 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -14,13 +14,20 @@ void dfs(int now){
     for(auto i : g[now]){
         if(vis[i])continue;
         dfs(i);
+        //test
+        //cout << "dfs " << i << "  end. dp[i][0], dp[i][1] : " << dp[i][0] <<", "<< dp[i][1]<<"\n";
         dp[now][0] += min(dp[i][1],dp[i][0]);
         dp[now][1] += dp[i][0];
+        //test
+        //cout<<"dp " << now << " : " << dp[now][0] << " , " << dp[now][1] << ".\n";
     }
+    //test
+    //cout << "finally, dp " << now << " : " << dp[now][0] << ", " << dp[now][1] << "\n";
 }
 
 int main()
 {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     cin>>n;
     for(int i = 1; i < n; i++){
         cin>>u>>v;
@@ -28,6 +35,10 @@ int main()
         g[v].push_back(u);
     }
     dfs(1);//루트노드인 1부터 dfs시작
+
+    // for (int i = 1; i <= n; i++)cout<<dp[i][0]<<" "<<dp[i][1] << ", ";
+    // cout<<endl;
+
     cout<<min(dp[1][0],dp[1][1]);
     return 0;
 }   
