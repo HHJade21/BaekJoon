b6a3585bf616fd4c33f9f17a56902d25e082275b
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-02-15 14:33:42 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -6,72 +6,44 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int m, n, min = 100000000, bwcount, wbcount;
-
-    cin >> m >> n;
-
-    vector<vector<int>> map(m, vector<int>(n,0));
-    vector<vector<int>> bw(m, vector<int>(n,0));
-    vector<vector<int>> wb(m, vector<int>(n,0));
-
-    //map 채우기
-    for(int i = 0; i < m; i++){
-        string s;
-        cin >> s;
-        for(int j = 0; j < n; j++){
-            if(s[j]=='B')map[i][j]=1;
-            else map[i][j]=0;
-        }
-    }
-
-    //bw, wb 채우기
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            if(i%2==0){
-                if(j%2==1){
-                    bw[i][j]=0;
-                    wb[i][j]=1;
-                }
-                else{
-                    bw[i][j]=1;
-                    wb[i][j]=0;
-                }
+    char chess1[50][50], chess2[50][50];
+    string map[50];
+    int n,m, ans=100000000,sum1,sum2;
+    cin>>n>>m;
+    for(int i = 0; i < 50; i++){
+        for(int j = 0; j < 50; j++){
+            if((i+j)%2==1){
+                chess1[i][j]='W';
+                chess2[i][j]='B';
             }
             else{
-                if(j%2==0){
-                    bw[i][j]=0;
-                    wb[i][j]=1;
-                }
-                else{
-                    bw[i][j]=1;
-                    wb[i][j]=0;
-                }
+                chess1[i][j]='B';
+                chess2[i][j]='W';
             }
         }
     }
-
-
-    for(int i = 0; i <= m-8; i++){
-        for(int j = 0; j <=n-8; j++){
-            //잘랐으면 여기서 2중for문 돌면서 칠해야 될 사각형의 개수 count
-            bwcount=0;
-            wbcount=0;
-            for(int p = i; p<i+8; p++){
-                for(int q = j; q<j+8; q++){
-                    if(map[p][q] != bw[p][q]) bwcount++;
-                    else if(map[p][q]!=wb[p][q]) wbcount++;
-                }
-            }
-            //현재 8*8격자에서 새로 칠해야 될 칸의 개수 모두 구한 상태.
-            //min이랑 비교해서 갱신
-            //min = 8, 
-            //bwcount = 6
-            //wbcount = 4
-            if(min>bwcount)min=bwcount;
-            if(min>wbcount)min=wbcount;
-        }
+    for(int i = 0; i <n; i++){
+        cin>>map[i];
     }
 
-    cout<<min;
+    for(int i = 0; i <= n-8; i++){
+        for(int j = 0; j <= m-8; j++){
+            //여기서부터 8*8사이즈로 체스판 답안 1 2랑 주어진 지도랑 비교
+            sum1 = 0;
+            sum2 = 0;
+            for(int k = 0; k < 8; k++){
+                for(int l = 0; l < 8; l++){
+                    if(map[i+k][j+l]!=chess1[i+k][j+l]){
+                        sum1++;
+                    }
+                    if(map[i+k][j+l]!=chess2[i+k][j+l]){
+                        sum2++;
+                    }
+                }
+            }
+            ans = min(ans, sum1);
+            ans = min(ans, sum2);
+        }
+    }
+    cout << ans;
 }
