b95831b60e8b4e23ba544eaf93c278eea6555c1e
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-02-20 12:31:58 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.cc
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -9,17 +9,11 @@ int main(){
     bool print = false;
     cin >> s;
     stack<char>st;
-    queue<char>a;
     for (int i = 0; i < s.size(); i++){
         if(s[i]>='A'&&s[i]<='Z'){
-            a.push(s[i]);
-            continue;
+            cout<<s[i];
         }
         else if(s[i]=='+'||s[i]=='-'){//더하기빼기
-            while(a.size()){
-                cout << a.front();
-                a.pop();
-            }
             while(st.size() && st.top()!='('){
                 cout << st.top();
                 st.pop();
@@ -27,10 +21,6 @@ int main(){
             st.push(s[i]);
         }
         else if(s[i]=='*'||s[i]=='/'){//곱하기나누기
-            while(a.size()){
-                cout << a.front();
-                a.pop();
-            }
             if(st.size() && (st.top()=='*' || st.top()=='/')){
                 cout << st.top();
                 st.pop();
@@ -41,10 +31,6 @@ int main(){
             st.push(s[i]);
         }
         else{//닫는괄호
-            while(a.size()){
-                cout << a.front();
-                a.pop();
-            }
             while(st.size() && st.top()!='('){
                 cout << st.top();
                 st.pop();
@@ -52,10 +38,6 @@ int main(){
             st.pop();
         }   
     }
-    while(a.size()){
-                cout << a.front();
-                a.pop();
-            }
     while(st.size()){
         if(st.top()!='(')cout<< st.top();
         st.pop();
