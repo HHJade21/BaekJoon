2b9b1c371692164d2e03cebddd0c21a2f4d6ff8b
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-03-21 21:11:51 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 68 ms, Memory: 4088 KB -BaekjoonHub

---
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<limits.h>
+using namespace std;
+
+int n,a,b;
+int p[300001];
+
+int find(int x){
+    if(p[x]==x)return x;
+    return p[x] = find(p[x]);
+}
+
+void unionfind(int x, int y){
+    x = find(x);
+    y = find(y);
+    p[y]=x;
+}
+
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin>>n;
+    for(int i = 1; i <=n ;i++)p[i]=i;
+    for(int i = 1; i < n; i++){
+        cin>>a>>b;
+        unionfind(a,b);
+    }
+    a=find(1);
+    b=0;
+    for(int i = 1; i <=n; i++){
+        if(find(i)!=a){
+            b=find(i);
+            break;
+        }
+    }
+    cout << a << " " << b;
+}
