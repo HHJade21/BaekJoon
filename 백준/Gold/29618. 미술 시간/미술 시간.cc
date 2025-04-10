8ccc129469ee4a97f67bc5d7c7fb91a400b161ab
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-04-10 20:31:48 +0900

    [Gold III] Title: 미술 시간, Time: 44 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Gold/29618. 미술 시간/미술 시간.cc
+++ 백준/Gold/29618. 미술 시간/미술 시간.cc
@@ -1,47 +1,34 @@
 #include<iostream>
 #include<vector>
+#include<queue>
+#include<algorithm>
+#include<limits.h>
 using namespace std;
+int a,b,x,n,q,p[100001],color[100001];
 
-int p[100002] = {0,};
-int c[100002] = {0,};
-
-int find(int a){
-    if(p[a] != a){
-        return p[a] = find(p[a]);
-    }
-    else if(c[a+1]){
-        p[a+1] = find(p[a+1]);
-        return p[a] = p[a+1];
-    }
-    else{
-        return a;
-    }
+int find(int x){
+    if(p[x]==x)return x;
+    return p[x]=find(p[x]);
 }
 
+void unioon(int x, int y){
+    x=find(x);
+    y=find(y);
+    if(x>y)swap(x,y);//x가 작도록 맞춰주기
+    p[x]=y;
+}
 
 int main(){
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int n, q, a, b, x, y;
-    cin >> n >> q;
-    for(int i = 0; i <= n+1; i++){
-        p[i] = i;
-    }
-
-    for(int i = 0; i < q; i++){
-        cin >> a >> b >> x;
-        y = find(b);
-        for(int j = a; j <= b; j++){
-            if(c[j]){
-                j = find(p[j]);
-                continue;
-            }
-            c[j] = x;
-            p[j] = y;
+    cin>>n>>q;
+    for(int i = 1; i <= n; i++)p[i]=i;
+    while(q--){
+        cin>>a>>b>>x;
+        for(int i = a; i<=b; i++){
+            if(color[i-1])unioon(i-1, i);
+            if(color[i])i=find(i);
+            else color[i]=x;
         }
     }
-
-    for(int i = 1; i <= n; i++){
-        cout << c[i] << " ";
-    }
+    for(int i = 1; i<=n; i++) cout << color[i] << " ";
 }
-
