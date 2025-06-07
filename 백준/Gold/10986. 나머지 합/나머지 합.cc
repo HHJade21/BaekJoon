e026bfb609abd3300159ff94c3b88b40258106f4
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-06-07 14:06:43 +0900

    [Gold III] Title: 나머지 합, Time: 100 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -1,14 +1,13 @@
 #include<iostream>
 using namespace std;
-
 int main(){
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     long long tmp, sum=0,res[1000]={1,},n,m,cnt=0;
     cin>>n>>m;
     while(n--){
         cin>>tmp;
-        sum+=tmp%m;
-        cnt += res[sum%m]++;
+        sum = (sum + tmp)%m;
+        cnt += res[sum]++;
     }
     cout<<cnt;
 }
