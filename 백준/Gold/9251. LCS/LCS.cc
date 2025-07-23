75a067183000f0c57167863dac1d93b5a000bca0
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-07-23 23:22:23 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5816 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,24 +1,15 @@
 #include<iostream>
-#include<vector>
 using namespace std;
-
 int main(){
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     string a,b;
-
-    cin >> a >> b;
-
-    vector<vector<int>>dp(b.size()+1, vector<int>(a.size()+1, 0));
-
-    for(int i = 1; i <= b.size(); i++){
-        for(int j = 1; j <= a.size(); j++){
-            if(a[j-1]==b[i-1]){
-                dp[i][j]=dp[i-1][j-1]+1;
-            }
-            else {
-                dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
-            }
+    cin>>a>>b;
+    int as = a.size(), bs=b.size(), dp[1001][1001]={};
+    for(int i = 1; i <= as; i++){//문자열 a의 i번째 글자가 추가됐을 때
+        for(int j = 1; j <= bs; j++){//b의 j번째 글자 추가
+            if(a[i-1]==b[j-1]) dp[i][j]=dp[i-1][j-1]+1;
+            else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
         }
     }
-    cout << dp[b.size()][a.size()];
+    cout<<dp[as][bs];
 }
