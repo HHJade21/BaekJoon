9d8d982270b0ec5caf7e0abdcbc8931ce9eb0614
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-08-13 21:35:16 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<map>
+using namespace std;
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	map<string, int> clothes;
+
+	string temp, tempKey;
+	int t, n, result = 1;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		result = 1;
+		for (int j = 0; j < n; j++) {
+			cin >> temp >> tempKey;
+			auto category = clothes.find(tempKey);
+			if (category != clothes.end()) {
+				category->second++;
+			}
+			else {
+				clothes.insert({ tempKey, 1 });
+			}
+		}
+		for (auto iter = clothes.begin(); iter != clothes.end(); iter++) {
+			result *= (iter->second) + 1;
+		}
+		cout << result - 1 << "\n";
+
+		clothes.clear();
+	}
+}
