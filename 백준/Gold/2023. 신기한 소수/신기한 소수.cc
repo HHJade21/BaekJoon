ae7ff71243ab05bacf4262c0c2667306439b7040
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-09-24 22:00:16 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.cc
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -1,24 +1,16 @@
 #include<iostream>
 using namespace std;
 int n;
-
 void amazing(int now, int depth){
     for(int i = 2; i*i<=now; i++){
         if(now%i==0)return;
     }
-
     if(depth==n) cout << now << "\n";
     else{
-        amazing(now*10+1, depth+1);
-        amazing(now*10+3, depth+1);
-        amazing(now*10+7, depth+1);
-        amazing(now*10+9, depth+1);
+        for(int i = 1; i<=9; i+=2)amazing(now*10+i, depth+1);
     }
 }
 int main(){
     cin>>n;
-    amazing(2,1);
-    amazing(3,1);
-    amazing(5,1);
-    amazing(7,1);
+    for(int i = 2; i<=7; i++)amazing(i,1);
 }
