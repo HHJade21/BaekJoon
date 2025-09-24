a678d2be5d508afec798ffeb8baedd5db4484065
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2025-09-24 21:36:06 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.cc
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -1,38 +1,24 @@
 #include<iostream>
-#include<vector>
-#include<queue>
-#include<algorithm>
-#include<limits.h>
-#include<cmath>
 using namespace std;
 int n;
 
-void amazing(string s){
-    int tmp = stoi(s);
-    for(int k = 2; k <= sqrt(tmp);k++){
-        if(tmp%k==0){
-            return;
-        }
+void amazing(int now, int depth){
+    for(int i = 2; i*i<=now; i++){
+        if(now%i==0)return;
     }
-    //s가 소수인 경우
-    if(s.size()==n){
-        cout << s << "\n";
-    }
+
+    if(depth==n) cout << now << "\n";
     else{
-        amazing(s+"1");
-        amazing(s+"3");
-        amazing(s+"7");
-        amazing(s+"9");
+        amazing(now*10+1, depth+1);
+        amazing(now*10+3, depth+1);
+        amazing(now*10+7, depth+1);
+        amazing(now*10+9, depth+1);
     }
 }
-
 int main(){
-    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     cin>>n;
-
-    amazing("2");
-    amazing("3");
-    amazing("5");
-    amazing("7");
-
+    amazing(2,1);
+    amazing(3,1);
+    amazing(5,1);
+    amazing(7,1);
 }
