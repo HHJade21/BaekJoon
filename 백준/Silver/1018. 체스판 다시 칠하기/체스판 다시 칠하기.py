c2eef486aadb6f4660153209cb772b557345a5a1
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2026-01-15 16:30:26 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 104 ms, Memory: 110760 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,36 @@
+bw = []
+wb = []
+
+for i in range(50):
+    if i%2 == 0 :
+        bw.append("BW"*25)
+        wb.append("WB"*25)
+    else:
+        bw.append("WB"*25)
+        wb.append("BW"*25)
+
+
+n,m=map(int,input().split())
+chess = []
+
+for i in range(n):
+    chess.append(input())
+
+ans = 64
+
+for i in range(n-7):
+    for j in range(m-7):
+        bwCnt = 0
+        wbCnt = 0
+        for p in range(8):
+            for q in range(8):
+                if bw[p][q] != chess[i+p][j+q]:
+                    bwCnt+=1
+                else:
+                    wbCnt+=1
+        if bwCnt < ans :
+            ans = bwCnt 
+        if wbCnt < ans:
+            ans = wbCnt
+
+print(ans)
