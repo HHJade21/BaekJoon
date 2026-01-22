918fa2794532e31f28d16e1f4568c2e72b28bd4b
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2026-01-22 21:42:55 +0900

    [Gold V] Title: 탑, Time: 88 ms, Memory: 8072 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,68 +1,40 @@
 #include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<limits.h>
 using namespace std;
-#define STACK_SIZE 500000
-int hstack[STACK_SIZE]; //왼큰수 판별용 스택. 타워 높이 저장
-int nstack[STACK_SIZE]; //타워 번호 저장
-int top = -1;
 
-void push(int a, int b) {
-	hstack[++top] = a;
-	nstack[top] = b;
-}
+int main(){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int n;
+    cin>>n;
+    vector<int> tower(n);
+    vector<int> ans(n,0);
+    stack<int> st;
+    cin>>tower[0];
+    ans[0]=-1;
+    st.push(0);
 
-void pop() {
-	top--;
-}
+    for(int i = 1; i < n; i++){
+        cin >> tower[i];
+        
+        if(tower[i-1] >= tower[i]){
+            ans[i] = i-1;
+            st.push(i);
+        }
+        else{
+            while(st.size()>0 && tower[st.top()] < tower[i]) st.pop();
 
-int main() {
+            if(st.size()>0) ans[i] = st.top();
+            else ans[i] = -1;
 
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+            st.push(i);
+        }
+    }
 
-
-	int N, maxNum;
-
-	cin >> N;
-	int* tower = new int[N]; // 각 탑의 높이를 받는 배열
-	int* receiver = new int[N]; //해당 타워의 신호를 수신한 탑의 번호를 저장하는 배열. 마지막에 얘를 출력함.
-	for (int i = 0; i < N; i++) {
-		cin >> tower[i];
-	}
-
-	receiver[0] = 0; // 첫 타워는 신호가 날라가니 0
-	maxNum = tower[0]; // 현재까지 등장한 타워의 최대값은 0번 타워의 높이
-	push(tower[0], 1);
-
-	if (N > 1) {
-		for (int i = 1; i < N; i++) {
-			if (tower[i] > maxNum) {
-				receiver[i] = 0;
-				maxNum = tower[i];
-				push(tower[i], i + 1);
-			}
-			else {
-				if (tower[i-1] > tower[i]) {
-					receiver[i] = i; //i-1번 인덱스, 즉 i번째 타워에서 얘를 수신하니까.
-					push(tower[i], i + 1);
-				}
-				else if (tower[i - 1] == tower[i]) {
-					receiver[i] = receiver[i - 1];
-				}
-				else if (tower[i - 1] < tower[i]) {
-					while (tower[i] >= hstack[top]) {
-						pop();
-					}
-					receiver[i] = nstack[top];
-					push(tower[i], i + 1);
-				}
-			}
-		}
-	}
-	for (int i = 0; i < N; i++) {
-		cout << receiver[i] << " ";
-	}
-
-	delete[] tower;
-	delete[] receiver;
+    for(int i = 0; i < n; i++){
+        cout << ans[i]+1 << " ";
+    }
 }
