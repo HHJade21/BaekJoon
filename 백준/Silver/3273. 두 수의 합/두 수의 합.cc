76f0fb5c53a8cb2124f3ff545490b7ec69bf8b38
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2026-02-28 09:46:41 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -6,26 +6,22 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int n,x,cnt=0;
-    cin >> n;
-    vector<int>a(n);
-    for(int i = 0; i < n; i++)cin>>a[i];
-    cin>>x;
-    sort(a.begin(), a.end());
-
-    int l = 0, r = a.size()-1, sum = 0;
-
-    while(l<r){
-        sum=a[l]+a[r];
-        if(sum==x)cnt++;
-        
-        if(sum<=x){
-            l++;
-        }
-        else{
-            r--;
-        }
-    }
-    cout<<cnt;
+	ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	int n,ans=0,x;
+	cin>>n;
+	vector<int>v(n);
+	for(int i = 0; i < n; i++){
+		cin>>v[i];
+	}
+	cin>>x;
+	sort(v.begin(),v.end());
+	int left=0, right = n-1;
+	while(left<right){
+		while(v[left]+v[right] > x)right--;
+		if(left>=right)break;
+		if(v[left]+v[right]==x)ans++;
+		
+		left++;
+	}
+	cout<<ans;
 }
