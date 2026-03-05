10345e301ee7f084204a47c6e7c36bee4dc7c885
HHJade21 <92563272+HHJade21@users.noreply.github.com>
2026-03-05 20:07:41 +0900

    [Platinum V] Title: 연세워터파크, Time: 16 ms, Memory: 5220 KB -BaekjoonHub

--- 백준/Platinum/15678. 연세워터파크/연세워터파크.cc
+++ 백준/Platinum/15678. 연세워터파크/연세워터파크.cc
@@ -1,19 +1,19 @@
 #include<iostream>
 #include<queue>
-#include<limits.h>
 using namespace std;
 int main(){
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    //{i번 다리를 마지막으로 밟을 때의 최대점수, 해당 다리 번호 i}
-    priority_queue<pair<long long,int>>pq;
-    long long n,d,ans=LLONG_MIN, tmp;
+    priority_queue<pair<long long,int>>pq;//최대점수, 계단번호 순으로 저장할 최대우선순위큐
+    long long n,d,ans=-100000000000000, tmp;
     cin>>n>>d;
     for(int i = 0; i < n; i++){
         cin>>tmp;//i번째 징검다리의 점수 입력
-        while(pq.size() && pq.top().second < i-d)pq.pop();
-        if(pq.size())tmp += pq.top().first;
-        if(tmp>0)pq.push({tmp,i});
-        ans = max(ans,tmp);
+        while(pq.size() && pq.top().second < i-d)pq.pop();//d칸 이상 멀어진 징검다리들 정보 삭제
+        if(pq.size())tmp += pq.top().first;//앞에 총점수가 양수인 징검다리가 있으면 합산
+        if(tmp>0)pq.push({tmp,i});//현재 계단을 밟고 얻을 수 있는 최종점수가 양수면 pq에 삽입
+        ans = max(ans,tmp);//정답 갱신
     }
-    cout<<ans;
+    cout<<ans;//출력
 }
+
+
